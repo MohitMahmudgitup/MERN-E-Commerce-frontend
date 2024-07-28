956ba229c61d8b43d09a99b9189aac9d1dc936e8
MohitMahmudgitup <mohitmahmud25@gmail.com>
2024-07-29 00:16:54 +0600

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from "./components/header"
+import Footer from "./components/footer"
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Header/>
+
+      <main className='min-h-[calc(100vh-138px)]'>
+        <Outlet/>
+      </main>
+      
+      <Footer/>
+    </>
   );
 }
 
